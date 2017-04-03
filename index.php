46265c19b0d77bbfe34bcc5d75d48780e9e425c3
AdamDiment <adam_diment@hotmail.com>
2017-04-03 11:52:02 +0300

    Add daily proclamation

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<?php echo "Hello world this is shalomify shabba"; ?> 
+<?php echo "Hello world this is shalomify shabba"; ?>
+
+
+<html>
+<head>
+
+</head>
+<body>
+<h1>Daily Proclamation</h1>
+<p>My body is a  for the Holy Spirit</p>
+<p>Redeemed, cleansed and sanctified by the blood of Jesus</p>
+<p>My members, the parts of my body, are instruments of righteousness, yielded to God for His service and for His glory</p>
+<p>The devil has no place in me, no power over me, no unsettled claims against me</p>
+<p>All has been settled by the blood of Jesus</p>
+<p>I overcome satan by the blood of the lamb and by the word of my testimony and I do not love my life to the death</p>
+<p>My body is for the Lord, and the Lord is for my body.</p>
+<p>Amen.</p>
+
+</body>
+</html> 
